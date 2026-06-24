f19b57a7cba50363910776adb6cfc698445fe727
valeramirezca <valeramirez.ca@gmail.com>
2026-06-24 15:44:10 -0400

    Add files via upload

---
+++ PeliculasYDirectores_Valentina/PeliculasYDirectores2/src/test/java/com/valentina/PeliculasYDirectoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasYDirectoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
